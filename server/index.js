3edd701fb7cca2205be62d912864e57a73e8d571
Suyash Singh <suyash@Suyashs-MacBook-Pro-2.local>
2020-10-06 12:11:51 -0400

    database initial setup done

--- server/index.js
+++ server/index.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const pool = require('./db');
+
+// middleware
+app.use(cors());
+app.use(express.json()); // in a full stack app the only way to..
+// get data from the client side is to
+// get it from the req.body object.
+
+// Routes
+
+// create a todo
+
+// get all todos
+
+// get a todo
+
+// update a todo
+
+// delete a todo
+
+//
 
 app.listen(5000, () => {
-  console.log('testing 123');
+  console.log('server running');
 });
