dc0b4c5423de5985db1537489ec9d270a773e307
Suyash Singh <suyash@Suyashs-MacBook-Pro-2.local>
2020-10-08 18:00:51 -0400

    edit and delete deatures complete

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,13 +1,30 @@
 import React, { Fragment, useEffect, useState } from 'react';
+import EditTodo from './EditTodo';
 
 const ListTodos = () => {
   const [todos, setTodo] = useState([]);
 
-  const getTodo = async () => {
-    const response = await fetch('http://localhost:5000/todos');
-    const jsonData = await response.json();
+  const deleteTodo = async (id) => {
+    try {
+      const deletedTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: 'Delete'
+      });
 
-    setTodo(jsonData);
+      setTodo(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const getTodo = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/todos');
+      const jsonData = await response.json();
+
+      setTodo(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
   };
 
   useEffect(() => {
@@ -16,7 +33,7 @@ const ListTodos = () => {
 
   return (
     <Fragment>
-      <table className='table w-50 m-auto '>
+      <table className='table w-75 m-auto '>
         <thead>
           <tr>
             <th>Description</th>
@@ -28,8 +45,17 @@ const ListTodos = () => {
           {todos.map((todo) => (
             <tr key={todo.todo_id}>
               <td>{todo.description}</td>
-              <td>Edit</td>
-              <td>Delete</td>
+              <td>
+                <EditTodo todo={todo} />
+              </td>
+              <td>
+                <button
+                  className='btn btn-danger'
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
